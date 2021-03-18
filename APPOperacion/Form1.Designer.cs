c4a762aa36fd8fb82b2fdda0251860e19f8c5df7
loquillosay <jdcastao4@misena.edu.co>
2021-03-18 04:45:43 -0500

    initial commit

---
+++ APPOperacion/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace APPOperacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtsuma = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(554, 42);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(150, 31);
+            this.txt1.TabIndex = 0;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(554, 199);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(150, 31);
+            this.txt4.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(554, 100);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(150, 31);
+            this.txt2.TabIndex = 2;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(554, 149);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(150, 31);
+            this.txt3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(443, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nota_1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(443, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Nota_2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(443, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Nota_3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(443, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Nota_4";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(390, 251);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(112, 34);
+            this.btnCalcular.TabIndex = 11;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtsuma
+            // 
+            this.txtsuma.Location = new System.Drawing.Point(554, 300);
+            this.txtsuma.Name = "txtsuma";
+            this.txtsuma.Size = new System.Drawing.Size(150, 31);
+            this.txtsuma.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(443, 306);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtsuma);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtsuma;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ APPOperacion/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Tex;
+
+namespace APPOperacion
+{
+    public partial class Form1 : Form
+    {
+        private List<float> _notes = new List<float>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         private void addNote(float note) {
+            _notes.Add(note);
+        } 
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //instanciar la logica de negocio 
+            Operacion objO = new Operacion();
+
+            try
+            {
+
+                addNote(float.Parse(txt1.Text));
+                addNote(float.Parse(txt2.Text));
+                addNote(float.Parse(txt3.Text));
+                addNote(float.Parse(txt4.Text));
+
+                objO.setNotas=_notes;
+
+                if (objO.calcular())
+                {
+                    txtsuma.Text = objO.getSum.ToString();
+                    return;
+                }
+                MessageBox.Show(objO.getError);
+                _notes.Clear();
+                objO = null;
+            }
+            catch(Exception _)
+            {
+                txtsuma.Text = "No puede ser Texto";
+            }   
+        }
+
+    }
+}
